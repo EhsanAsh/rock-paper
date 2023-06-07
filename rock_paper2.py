1d6c092a6347a9be73ea68f5eba76fd0682b85c2
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-06-07 00:36:36 -0400

    modify if condition and emojis-finished

--- rock_paper2.py
+++ rock_paper2.py
@@ -1,4 +1,3 @@
-# already has done some work/ the next step is to add for loop to make it run 3 times.
 import random
 from emoji import emojize
 print(emojize(':gem_stone:'))
@@ -6,58 +5,56 @@ print(emojize(':newspaper:'))
 print(emojize(':scissors:'))
 player_wins = 0
 computer_wins = 0
-rounds = 1
-while rounds <= 3:
-    player = input('Player, Please enter your choice:\n')
-    computer = random.randint(0, 2)
-    if player:
-        rounds += 1
-        if player_wins == 2:
-            print(
-                f'Player {player_wins}, computer {computer_wins}. Player wins the game!' +
-                emojize(':star_struck:') + emojize(':flexed_biceps:')
-            )
-            break
-        elif computer_wins == 2:
-            print(f'Player {player_wins}, computer {computer_wins}. Computer wins the game!' + emojize(':🤦‍♀️:') + emojize(':🤷‍♀️:')
-            )
-            break
-        else:
-            if player == 'rock':
-                if computer == 0:
-                    print('AI chose rock')
-                    print('It is a tie!')
-                elif computer == 1:
-                    print('AI chose paper')
-                    print('computer wins!')
-                    computer_wins += 1
-                else:
-                    print('AI chose scissors')
-                    print('player wins!')
-                    player_wins += 1
-            if player == 'paper':
-                if computer == 0:
-                    print('AI chose rock')
-                    print('player wins!')
-                    player_wins += 1
-                elif computer == 1:
-                    print('AI chose paper')
-                    print('It is a tie!')
-                else:
-                    print('AI chose scissors')
-                    print('computer wins!')
-                    computer_wins += 1
-            if player == 'scissors':
-                if computer == 0:
-                    print('AI chose rock')
-                    print('computer wins!')
-                    computer_wins += 1
-                elif computer == 1:
-                    print('AI chose paper')
-                    print('player wins!')
-                    player_wins += 1
-                else:
-                    print('AI chose scissors')
-                    print('It is a tie!')
+while True :
+    if player_wins == 2:
+        print(
+            f'Player {player_wins}, computer {computer_wins}. Player wins the game!' +
+            emojize('🤩') + emojize('💪')
+        )
+        break
+    elif computer_wins == 2:
+        print(f'Player {player_wins}, computer {computer_wins}. Computer wins the game!' + emojize('🤦‍♀️') + emojize('🤷‍♀️')
+        )
+        break
     else:
-        print('Player did not enter his \ her choice correctly. Please enter your choice again!')
+        player = input('Player, Please enter your choice:\n')
+        computer = random.randint(0, 2)
+        if player:
+                if player == 'rock':
+                    if computer == 0:
+                        print('AI chose rock')
+                        print('It is a tie!')
+                    elif computer == 1:
+                        print('AI chose paper')
+                        print('computer wins!')
+                        computer_wins += 1
+                    else:
+                        print('AI chose scissors')
+                        print('player wins!')
+                        player_wins += 1
+                if player == 'paper':
+                    if computer == 0:
+                        print('AI chose rock')
+                        print('player wins!')
+                        player_wins += 1
+                    elif computer == 1:
+                        print('AI chose paper')
+                        print('It is a tie!')
+                    else:
+                        print('AI chose scissors')
+                        print('computer wins!')
+                        computer_wins += 1
+                if player == 'scissors':
+                    if computer == 0:
+                        print('AI chose rock')
+                        print('computer wins!')
+                        computer_wins += 1
+                    elif computer == 1:
+                        print('AI chose paper')
+                        print('player wins!')
+                        player_wins += 1
+                    else:
+                        print('AI chose scissors')
+                        print('It is a tie!')
+        else:
+            print('Player did not enter his \ her choice correctly. Please enter your choice again!')
